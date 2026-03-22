886ca1060e0aceff3b449936627353138d3d77eb
ARJ2211 <aayushrj22@gmail.com>
2026-03-21 20:37:03 -0400

    Removed spawnChild as the worker issue has been fixed

--- backend/workers/worker.js
+++ backend/workers/worker.js
@@ -48,55 +48,6 @@ const spawnWorker = (workerFile, data, onComplete, onProgress) => {
 };
 
 /**
- * Same idea as spawnWorker but uses child_process.fork()
- * instead of worker_threads.
- *
- * We need this because @xenova/transformers internally uses
- * node:cluster which straight up does not work inside a
- * worker thread. fork() gives us a full separate Node process
- * where everything works normally.
- *
- * Communication is the same idea:
- *   worker_threads:  parentPort.postMessage()  /  worker.on("message")
- *   child_process:   process.send()            /  child.on("message")
- *
- * @param {string} workerFile one of the workers in workers/*.worker.js
- * @param {object} data data required for the worker
- * @param {function} onComplete simple callback
- * @param {function} [onProgress] optional callback for intermediate progress
- */
-const spawnChild = (workerFile, data, onComplete, onProgress) => {
-    const child = fork(join(__dirname, workerFile));
-
-    child.on("message", (msg) => {
-        if (msg.type === "progress") {
-            if (onProgress) onProgress(msg);
-        } else if (msg.type === "done") {
-            if (msg.success) {
-                onComplete(null, msg);
-            } else {
-                onComplete(new Error(msg.error));
-            }
-        }
-    });
-
-    child.on("error", (err) => {
-        onComplete(err);
-    });
-
-    child.on("exit", (code) => {
-        if (code !== 0 && code !== null) {
-            onComplete(new Error(`Child process exited with code ${code}`));
-        }
-    });
-
-    // Send the data to kick it off
-    // Unlike spawnWorker where data goes through workerData,
-    // fork() needs an explicit message to start
-    child.send({ type: "start", ...data });
-};
-
-/**
  * This is a simple test to see if workers work
  * @param {*} onComplete
  */
@@ -112,9 +63,6 @@ export const testWorker = (onComplete) => {
  * This is the ingest worker that will be used to
  * ingest books and then dump into Qdrant
  *
- * Uses spawnChild (fork) instead of spawnWorker because
- * @xenova/transformers blows up inside worker_threads
- * with "node:cluster does not provide an export named 'on'"
  *
  * @param {string} epubPath Path of the epub file on disk
  * @param {string} bookId MongoDB book ID
